4d17f961e0bd51317ff95f466984b0848517663c
Patrick R. Jordan <pjordan@reasoningtech.com>
2012-03-04 23:01:33 -0800

    adding homework 2.4 tests

---
+++ homework2.4/task.py
@@ -0,0 +1,7 @@
+# Define a procedure, countdown, that takes a
+# positive whole number as its input, and prints 
+# out a countdown from that number to 1, 
+# followed by Blastoff! 
+
+def countdown():
+	# Your code here
