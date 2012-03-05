f940a8ab90d2f130752b9a8bd71c75519209d2e0
Patrick R. Jordan <pjordan@reasoningtech.com>
2012-03-04 21:09:38 -0800

    added homework 2.1 unit test

---
+++ homework2.1/task.py
@@ -0,0 +1,13 @@
+# Define a procedure, udacify, that takes as 
+# input a string, and returns a string that 
+# is an uppercase 'U' followed by the input string.
+# for example, when we enter
+
+# print udacify('dacians')
+
+# the output should be the string 'Udacians'
+
+# Make sure your procedure has a return statement.
+
+def udacify():
+    # Your code here
