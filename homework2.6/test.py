175314bc1975830a0cbe6e97b26d33fcac473000
Patrick R. Jordan <pjordan@reasoningtech.com>
2012-03-04 23:49:24 -0800

    fixed homework 2.6 test typo

--- homework2.6/test.py
+++ homework2.6/test.py
@@ -3,7 +3,13 @@ import task
 
 class TestSequenceFunctions(unittest.TestCase):
     def test_dataset1(self):
-        self.assertEquals(find_last('aaaa', 'a'),3)
+        self.assertEquals(task.find_last('aaaa', 'a'),3)
+
+    def test_dataset2(self):
+        self.assertEquals(task.find_last('aaaa', 'c'),-1)
+
+    def test_dataset3(self):
+        self.assertEquals(task.find_last('aaab', 'b'),3)
 
 if __name__ == '__main__':
     unittest.main()
