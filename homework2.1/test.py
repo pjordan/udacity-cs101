9f7e6c8aca399f226a78ed14856ff3e54a4d71b1
Patrick R. Jordan <pjordan@reasoningtech.com>
2012-03-04 21:33:45 -0800

    fixed 2.1 <-> 2.3 test switch

--- homework2.1/test.py
+++ homework2.1/test.py
@@ -3,31 +3,10 @@ import task
 
 class TestSequenceFunctions(unittest.TestCase):
     def test_dataset1(self):
-        self.assertEquals(task.median(1,2,3),2)
+        self.assertEquals(task.udacify('dacity'),'Udacity')
 
     def test_dataset2(self):
-        self.assertEquals(task.median(3,2,1),2)
-
-    def test_dataset3(self):
-        self.assertEquals(task.median(2,3,1),2)
-
-    def test_dataset4(self):
-        self.assertEquals(task.median(2,1,3),2)
-
-    def test_dataset5(self):
-        self.assertEquals(task.median(3,1,2),2)
-
-    def test_dataset6(self):
-        self.assertEquals(task.median(1,3,2),2)
-
-    def test_dataset7(self):
-        self.assertEquals(task.median(1,3,1),1)
-
-    def test_dataset8(self):
-        self.assertEquals(task.median(1,3,3),3)
-
-    def test_dataset9(self):
-        self.assertEquals(task.median(3,3,3),3)
+        self.assertEquals(task.udacify('bob'),'Ubob')
 
 if __name__ == '__main__':
     unittest.main()
--- homework2.3/test.py
+++ homework2.3/test.py
@@ -3,10 +3,31 @@ import task
 
 class TestSequenceFunctions(unittest.TestCase):
     def test_dataset1(self):
-        self.assertEquals(task.udacify('dacity'),'Udacity')
+        self.assertEquals(task.median(1,2,3),2)
 
     def test_dataset2(self):
-        self.assertEquals(task.udacify('bob'),'Ubob')
+        self.assertEquals(task.median(3,2,1),2)
+
+    def test_dataset3(self):
+        self.assertEquals(task.median(2,3,1),2)
+
+    def test_dataset4(self):
+        self.assertEquals(task.median(2,1,3),2)
+
+    def test_dataset5(self):
+        self.assertEquals(task.median(3,1,2),2)
+
+    def test_dataset6(self):
+        self.assertEquals(task.median(1,3,2),2)
+
+    def test_dataset7(self):
+        self.assertEquals(task.median(1,3,1),1)
+
+    def test_dataset8(self):
+        self.assertEquals(task.median(1,3,3),3)
+
+    def test_dataset9(self):
+        self.assertEquals(task.median(3,3,3),3)
 
 if __name__ == '__main__':
     unittest.main()
